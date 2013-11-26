a450de213ae366f25509e80267be9c569b6dbd04
Phil Haack <haacked@gmail.com>
2013-11-25 19:45:53 -0800

    Initial script
    Still need to make sure we only grab published content. Also need to make sure we change the layout from 'post' to 'article' in the right cases.

---
+++ select-content-for-jekyll.sql
@@ -0,0 +1,22 @@
+SELECT
+FilePath = SUBSTRING(CONVERT(VARCHAR, DATEADD(hh, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) + '-' + c.EntryName + '.aspx.markdown',
+Content = 
+'---
+layout: post
+title: "' + REPLACE(c.Title, '"', '&quot;') + '"
+date: ' + SUBSTRING(CONVERT(VARCHAR, DATEADD(hh, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) + '
+comments: true
+categories: [' + 
+ ISNULL(SUBSTRING(
+   (SELECT ',' + cat.Title AS [text()]
+    FROM subtext_LinkCategories cat 
+    INNER JOIN subtext_Links l
+    ON l.CategoryID = cat.CategoryID
+    WHERE c.ID = l.PostID
+    FOR XML PATH ('')), 2, 1000), '') + ']
+---
+' + CAST(c.[Text] as NVARCHAR(MAX))
+FROM subtext_Content c
+INNER JOIN subtext_Config blog
+ON c.BlogID = blog.BlogID
+WHERE blog.BlogID = 0
