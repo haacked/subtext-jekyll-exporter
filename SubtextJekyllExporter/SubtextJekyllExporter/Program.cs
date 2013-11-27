7b324432d4dce94e1aa7cf5312b4658099a3aace
Haacked <haacked@gmail.com>
2013-11-27 15:05:17 -0800

    Add export command line program

---
+++ SubtextJekyllExporter/SubtextJekyllExporter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SubtextJekyllExporter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please pass a database name and an export directory.");
+                return;
+            }
+
+            string databaseName = args[0];
+            string rootDirectory = args[1];
+            string connectionString =
+            String.Format(@"Data Source=.\SQLEXPRESS;Initial Catalog={0};Integrated Security=True", databaseName);
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(GetExportSqlScript(), connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string filePath = reader.GetString(0).Replace(Environment.NewLine, "");
+                        string content = EscapeJekyllTags(reader.GetString(1));
+
+                        var path = Path.Combine(rootDirectory, filePath);
+                        EnsurePath(path);
+                        File.WriteAllText(path, content, Encoding.UTF8);
+                    }
+                }
+            }
+        }
+
+        private static string EscapeJekyllTags(string content)
+        {
+            return content
+                .Replace("{{", "{{ \"{{\" }}")
+                .Replace("{%", "{{ \"{%\" }}");
+        }
+
+        private static void EnsurePath(string path)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        private static string GetExportSqlScript()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            const string resourceName = "SubtextJekyllExporter.select-content-for-jekyll.sql";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- select-content-for-jekyll.sql
+++ select-content-for-jekyll.sql
@@ -1,13 +1,16 @@
 SELECT FilePath = (
-	CASE c.PostType 
-	WHEN 2 THEN 'articles/' + c.EntryName + '.aspx.markdown' 
-	ELSE SUBSTRING(CONVERT(VARCHAR, DATEADD(hh, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) + '-' + c.EntryName + '.aspx.markdown' 
-	END),
+    CASE c.PostType 
+    WHEN 2 THEN 'articles/' + c.EntryName + '.aspx.markdown' 
+    ELSE SUBSTRING(CONVERT(VARCHAR, DATEADD(HH, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) 
+        + '-' + c.EntryName + '.aspx.markdown' 
+    END),
 Content = 
 '---
 layout: ' + (CASE c.PostType WHEN 2 THEN 'page' ELSE 'post' END) + '
 title: "' + REPLACE(c.Title, '"', '&quot;') + '"
-date: ' + SUBSTRING(CONVERT(VARCHAR, DATEADD(hh, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) + '
+date: ' + SUBSTRING(CONVERT(VARCHAR, DATEADD(HH, blog.TimeZoneOffset, c.DatePublishedUtc), 120), 1, 10) + ' ' + 
+    (CASE WHEN blog.TimeZoneOffset < 0 
+    THEN '-' else '' END + RIGHT('00' + replace(blog.TimeZoneOffset, '-', ''), 2)) + '00
 comments: true
 categories: [' + 
  ISNULL(SUBSTRING(
@@ -18,6 +21,7 @@ categories: [' +
     WHERE c.ID = l.PostID
     FOR XML PATH ('')), 2, 1000), '') + ']
 ---
+
 ' + CAST(c.[Text] as NVARCHAR(MAX))
 FROM subtext_Content c
 INNER JOIN subtext_Config blog
